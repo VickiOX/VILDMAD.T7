d31ebc26563138a6137d992a573757156812a697
Zara <Zara>
2023-09-06 11:20:55 +0200

    skrevet i js (skelet)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-/*Teknisk dokumentation, eksempelvis i form af: 
+/*Teknisk dokumentation, eksempelvis i form af:
 ○ En overordnet forklaring af opbygningen og flowet i jeres scripts, gerne i form af diagrammer samt valgte navne-konventioner o.lign. 
-○ Forklarende kommentarer i koden (funktioner, variabler og hvordan disse anvendes) 
-○ Hvilke tiltag i har gjort ift. at gøre sitet mere bæredygtigt 
+○ Forklarende kommentarer i koden (funktioner, variabler og hvordan disse anvendes)
+○ Hvilke tiltag i har gjort ift. at gøre sitet mere bæredygtigt
 ○ Dokumentation af hvordan Git og branches er blevet brugt
 */
+
+const url = ""
+
+fetch(url)
+.then( res => res.json())
+.then(Show);
+
+function Show(){
+    console.log();
+}
 
 //mobil format først
