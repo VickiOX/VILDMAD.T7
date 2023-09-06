7702137b06fc204f25aafbb447f206560b79aac4
Zara <Zara>
2023-09-06 13:25:43 +0200

    små ændringer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*Teknisk dokumentation, eksempelvis i form af:
-○ En overordnet forklaring af opbygningen og flowet i jeres scripts, gerne i form af diagrammer samt valgte navne-konventioner o.lign. 
+○ En overordnet forklaring af opbygningen og flowet i jeres scripts, gerne i form af diagrammer samt valgte navne-konventioner o.lign.
 ○ Forklarende kommentarer i koden (funktioner, variabler og hvordan disse anvendes)
 ○ Hvilke tiltag i har gjort ift. at gøre sitet mere bæredygtigt
 ○ Dokumentation af hvordan Git og branches er blevet brugt
@@ -14,5 +14,44 @@ fetch(url)
 function Show(){
     console.log();
 }
+
+
+/* {
+  "id": 1533,
+  "gender": "Men",
+  "category": "Apparel",
+  "subcategory": "Topwear",
+  "articletype": "Tshirts",
+  "basecolour": "Red",
+  "season": "Fall",
+  "productionyear": 2010,
+  "usagetype": "Casual",
+  "productdisplayname": "Cat Red T-shirt",
+  "parsed": 1,
+  "soldout": 0,
+  "relid": 1533,
+  "price": 899,
+  "discount": null,
+  "variantname": "Cat",
+  "brandname": "Puma",
+  "brandbio": "PUMA is the World's Fastest Sports Brand",
+  "brandimage": "http://assets.myntassets.com/assets/images/2015/11/17/11447736932686-113016-3ff8sf.jpg",
+  "agegroup": "Adults-Men",
+  "colour1": "",
+  "colour2": "",
+  "fashiontype": "Fashion",
+  "materialcaredesc": null,
+  "sizefitdesc": null,
+  "styledesc": null
+} */
+
+
+
+
+
+
+
+
+
 
 //mobil format først
