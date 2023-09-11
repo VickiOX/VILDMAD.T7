510f36f3333dde9253106f16ff0ef351f2185d46
Zehra <zedu0001@stud.kea.dk>
2023-09-11 10:09:05 +0200

    burger menu

--- js/header.js
+++ js/header.js
@@ -10,5 +10,19 @@ window.addEventListener("scroll", function(){
     }
     lastScrollTop = scrollTop
 })
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 /* credit for the header code
 https://www.youtube.com/watch?v=JEBgqbZWYIQ */
