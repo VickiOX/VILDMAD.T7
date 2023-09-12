f3a72d2e4314a5761d47127e24faede03e6d7e13
VickiOx <viox0001@stud.kea.dk>
2023-09-12 22:46:31 +0200

    indsat text i info og indsat css hover + sammenlagt ikon og p

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function Show(){
 }
 
 
-fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/skovdata?select=id",{
+fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/Vildmad_produkter?select=*",{//brug * det henter alt
     method:"GET",
     headers:{
         apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyYm9iYmdrb2NxbXZ1YmVocXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTk5MTUsImV4cCI6MjAwOTU3NTkxNX0.Icz35OBcSiV2DSuLi9aszBlD1Bz2SJIqiTcaYc-9rSY"
@@ -26,7 +26,10 @@ fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/skovdata?select=id",{
 .then(showData);
 
 function showData(items){
-    console.log(items);
+    console.table(items);
+    items.forEach(item => {
+      console.log(item.name)
+    })
 }
 
 
@@ -70,13 +73,51 @@ function debugShowCategories(subcategory){
   const subcategory_template_copy = subcategory_template.cloneNode(true);
   subcategory_template_copy.querySelector("h2").textContent = subcategory;
 
-  const products = ['Kantarel','Bøg', 'Karl Johan', 'Skov jordbær', 'Skovfyr']; //erstat med fetch
+  fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/Vildmad_produkter?select=*&" + subcategory + "=eq.true&" + category + "=eq.true",{//brug * det henter alt
+  method:"GET",
+  headers:{
+      apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyYm9iYmdrb2NxbXZ1YmVocXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTk5MTUsImV4cCI6MjAwOTU3NTkxNX0.Icz35OBcSiV2DSuLi9aszBlD1Bz2SJIqiTcaYc-9rSY"
+  }
+})
+.then((res) => res.json())
+.then(products =>{
   products.forEach(product => {
     console.log(product);
     const product_template_copy = product_template.cloneNode(true);
-    product_template_copy.querySelector("h3").textContent= product;
+    product_template_copy.querySelector(".text2").id = product.id; //indsættelse af id til article
+    product_template_copy.querySelector("h3").textContent= product.name;
+    product_template_copy.querySelector(".text1").textContent= product.bio;
+    product_template_copy.querySelector(".text2").textContent= product.season;
+    product_template_copy.querySelector(".product_img").src = product.image;
+
+    product_template_copy.querySelector(".ikon1").setAttribute("data-id", product.id);
+    product_template_copy.querySelector(".ikon2").setAttribute("data-id", product.id);
+    product_template_copy.querySelector(".ikon3").setAttribute("data-id", product.id);
+    product_template_copy.querySelector(".ikon4").setAttribute("data-id", product.id);
+
+    product_template_copy.querySelector(".ikon1").onclick = function(ikon){
+      document.getElementById(ikon.currentTarget.getAttribute("data-id")).textContent = product.season;
+    }
+
+    product_template_copy.querySelector(".ikon2").onclick = function(ikon){
+      document.getElementById(ikon.currentTarget.getAttribute("data-id")).textContent = product.sankested;
+    }
+
+    product_template_copy.querySelector(".ikon3").onclick = function(ikon){
+      document.getElementById(ikon.currentTarget.getAttribute("data-id")).textContent = product.sankning;
+    }
+
+    product_template_copy.querySelector(".ikon4").onclick = function(ikon){
+      document.getElementById(ikon.currentTarget.getAttribute("data-id")).textContent = product.håndtering;
+    }
+
+    //appende
     subcategory_template_copy.querySelector(".grid").appendChild(product_template_copy);
+
   });
   //appende
   document.querySelector("main").appendChild(subcategory_template_copy);
+});
+
+  
 }
