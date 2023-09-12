8515b445b2d8df20df9b7261902cc07d1bfc1461
VickiOx <viox0001@stud.kea.dk>
2023-09-12 23:04:11 +0200

    tilføj kommentare i js

--- js/script.js
+++ js/script.js
@@ -5,70 +5,25 @@
 ○ Dokumentation af hvordan Git og branches er blevet brugt
 */
 
-const url = ""
-
-fetch(url)
-.then( res => res.json())
-.then(Show);
-
-function Show(){
-    console.log();
-}
-
-
-fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/Vildmad_produkter?select=*",{//brug * det henter alt
-    method:"GET",
-    headers:{
-        apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyYm9iYmdrb2NxbXZ1YmVocXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTk5MTUsImV4cCI6MjAwOTU3NTkxNX0.Icz35OBcSiV2DSuLi9aszBlD1Bz2SJIqiTcaYc-9rSY"
-    }
-})
-.then((res) => res.json())
-.then(showData);
-
-function showData(items){
-    console.table(items);
-    items.forEach(item => {
-      console.log(item.name)
-    })
-}
-
-
-function Show(){
-    console.log();
-}
-//fetch data og brug data som et array
-const subcategories = ['Løvskov', 'Nåleskov']; //erstat med fetch
 
  //fange template
  const subcategory_template = document.querySelector("#subcategory_template").content;
  const product_template = document.querySelector("#product_template").content;
 
-
+//breadcrumb is active, markere hvilken kategories der ses
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
 document.querySelector("h1").textContent = category
 document.querySelector("." + category).classList.add("active");
 
-// fetch
-fetch("https://kea-alt-del.dk/t7/api/products?limit=50&start=10&category=" + category)
-.then(res=>res.json())
-.then(showProducts);
 
+const subcategories = ['Løvskov', 'Nåleskov']; //hard code data
 function showProducts(){
-    //looper og kalder showProuct ental
-   // products.forEach(showProduct);
-
-
-    //template debugging
-    console.table(subcategories);
-    subcategories.forEach(debugShowCategories);
+   
+    subcategories.forEach(ShowCategoriesAndProducts);
 }
 
-function debugShowCategories(subcategory){
-  console.log(subcategory);
-
-
-
+function ShowCategoriesAndProducts(subcategory){
   //lave en kopi
   const subcategory_template_copy = subcategory_template.cloneNode(true);
   subcategory_template_copy.querySelector("h2").textContent = subcategory;
@@ -89,13 +44,18 @@ function debugShowCategories(subcategory){
     product_template_copy.querySelector(".text1").textContent= product.bio;
     product_template_copy.querySelector(".text2").textContent= product.season;
     product_template_copy.querySelector(".product_img").src = product.image;
-
-    product_template_copy.querySelector(".ikon1").setAttribute("data-id", product.id);
+  
+    // for at kunne ændre på text 2 via onclick skal vi have en identificator på click elementet (data-id)
+    //
+    product_template_copy.querySelector(".ikon1").setAttribute("data-id", product.id); 
     product_template_copy.querySelector(".ikon2").setAttribute("data-id", product.id);
     product_template_copy.querySelector(".ikon3").setAttribute("data-id", product.id);
     product_template_copy.querySelector(".ikon4").setAttribute("data-id", product.id);
 
-    product_template_copy.querySelector(".ikon1").onclick = function(ikon){
+    //onclick er js event og ikon er det element man klikker på
+    product_template_copy.querySelector(".ikon1").onclick = function(ikon){ 
+      //ikon.currentTarget bruger vi fordi at ikon ikke har attributter
+      // getElementById bruger vi fordi qs ikke virker med id'er som er numre
       document.getElementById(ikon.currentTarget.getAttribute("data-id")).textContent = product.season;
     }
 
