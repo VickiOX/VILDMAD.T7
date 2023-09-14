16363a1eed78cbcf72219e6716e670f54d526f5d
VickiOx <viox0001@stud.kea.dk>
2023-09-14 14:47:53 +0200

    test3

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 */
 
 fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/Vildmad_produkter?select=*",{//brug * det henter alt
+fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/Vildmad_produkter?select=*",{//brug * det henter alt
     method:"GET",
     headers:{
         apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyYm9iYmdrb2NxbXZ1YmVocXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTk5MTUsImV4cCI6MjAwOTU3NTkxNX0.Icz35OBcSiV2DSuLi9aszBlD1Bz2SJIqiTcaYc-9rSY"
@@ -15,6 +16,38 @@ fetch("https://jrbobbgkocqmvubehqtz.supabase.co/rest/v1/Vildmad_produkter?select
 .then(showData);
 
 function showData(items){
+    console.table(items);
+    items.forEach(item => {
+      console.log(item.name)
+    })
+}
+
+//fetch data og brug data som et array
+const subcategories = ['Løvskov', 'Nåleskov']; //erstat med fetch
+
+ //fange template
+ //const subcategory_template = document.querySelector("#subcategory_template").content;
+ const product_template = document.querySelector("#product_template").content;
+
+ //breadcrumb is active, markere hvilken kategories der ses
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category");
+document.querySelector("h1").textContent = category
+document.querySelector("." + category).classList.add("active");
+
+/* // fetch
+fetch("https://kea-alt-del.dk/t7/api/products?limit=50&start=10&category=" + category)
+.then(res=>res.json())
+.then(showProducts);
+ */
+/* showProducts()
+function showProducts(){
+   // products.forEach(showProduct);
+
+
+    //template debugging
+    console.table(subcategories);
+    subcategories.forEach(debugShowCategories);
     console.table(items);
     items.forEach(item => {
       console.log(item.name)
